812243eec48843018820c37f6a4b8036de9d8abf
garyb6 <gary.bennett6@outlook.com>
2022-02-21 14:33:59 +0000

    create employee test passed

--- src/test/java/com/employeeTracker/employeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/employeeTracker/employeeTracker/EmployeeTrackerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.employeeTracker.employeeTracker;
 
+import com.employeeTracker.employeeTracker.models.Employee;
+import com.employeeTracker.employeeTracker.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeTrackerApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee(){
+		Employee ben = new Employee("Ben Stokes", 30, 55, "ben@stokes.com");
+		employeeRepository.save(ben);
 	}
 
 }
